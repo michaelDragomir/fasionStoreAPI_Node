fcb61654220ae7c8bb40838cd57773296ff0f605
Michael Dragomir <michaeldragomir@Michaels-Laptop.lan>
2024-05-14 21:14:22 -0700

    update

--- app.ts
+++ app.ts
@@ -1,8 +1,15 @@
 import express, { Request, Response, NextFunction } from 'express';
+import path from 'path';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.get('/*', function (req: Request, res: Response) {
+	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.use((req: Request, res: Response, next: NextFunction) => {
 	res.status(200).json({ message: 'working' });
